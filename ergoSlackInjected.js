6b36b18248229602b6a00e00c4a5ea70b5b716d1
Kevin Burnett <kburnett@rosettastone.com>
2019-05-20 09:53:55 -0400

    also turn off d features when typing in a thread

--- ergoSlackInjected.js
+++ ergoSlackInjected.js
@@ -41,13 +41,21 @@ function markVisibleMessagesUnread() {
   }
 }
 
+function mainChatInputBoxEmpty() {
+  return $('#msg_input .ql-editor.ql-blank').length
+}
+
+function threadChatInputBoxEmpty() {
+  return $('.p-message_input_field .ql-editor.ql-blank').length
+}
+
 addEventListener("keydown", function(event) {
   // console.log('keydown', event.code);
   if (event.key === "d") {
     console.log('got d in ergoSlackInjected.js');
 
     // if not typing a message...
-    if ($('#msg_input .ql-editor.ql-blank').length) {
+    if (mainChatInputBoxEmpty() && threadChatInputBoxEmpty()) {
 
       // if the big "there are new messages" button is at the top of the "All Unreads" page, click it
       if ($('#channel_header_unread_refresh').length && !$('#channel_header_unread_refresh.hidden').length) {
